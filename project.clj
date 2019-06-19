426be9fd14f1310097136d90e8352b6ef028b83a
Ryan.K <ryankung@ieee.org>
2019-06-19 18:10:25 +0800

    first commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject jive "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [overtone "0.10.3"]]
+  :plugins [[lein-jupyter "0.1.16"]]
+)
